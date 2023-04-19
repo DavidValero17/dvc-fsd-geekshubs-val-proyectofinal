881cd2cb5538cc84d4ebdd64c62f29b4abe27ca5
David Valero <davidvalero17@hotmail.com>
2023-04-19 10:52:24 +0200

    añadida las conexiones en el archivo db.js y el config.json. Ya podemos realizar la conexión con la base de datos

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require("cors");
+const db = require("./db");
+const app = express();
+// const router = require('./router');
+//Importo el método de conexion a la base de datos
+
+app.use(cors());
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+// app.use(router)
+
+
+
+
+// const PORT =  process.env.PORT;
+
+const PORT =  4000;
+
+
+db.then(() => {
+    app.listen(PORT, () => console.log("Server on port " + PORT));
+})
+    .catch((err) => console.log(err.message));
+
+    
+
+
+
