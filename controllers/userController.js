e3a36c3e56557bb9dafcecb4d95b5a3a9506bc64
David Valero <davidvalero17@hotmail.com>
2023-05-05 00:37:09 +0200

    podemos actualizar los videojuegos como admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,7 @@ userController.getAllUsers = async (req, res) => {
         });
         return res.json({
             success: true,
-            message: "Here are all the clients",
+            message: "Here are all the users",
             data: allUsers
         });
     } catch (error) {
@@ -46,6 +46,46 @@ userController.addVideogame = async (req, res) => {
         );
     } catch (error) {
 
+        return res.status(500).json(
+            {
+                success: false,
+                message: "something went wrong",
+                error: error.message
+            }
+        );
+    }
+};
+
+userController.updateVideogame = async (req, res) => {
+    try {
+        const videogame_id = req.params.id;
+        const title = req.body.title;
+        const image = req.body.image;
+        const description = req.body.description;
+        const genre = req.body.genre;
+        const year = req.body.year;
+        const multiplayer = req.body.multiplayer;
+        const online = req.body.online;
+        const developer_id = req.body.developer_id;
+
+        const updateVideogame = await Videogame.update({ 
+            title: title,
+            image: image,
+            description: description,
+            genre: genre,
+            year: year,
+            multiplayer: multiplayer,
+            online: online,
+            developer_id: developer_id }, { where: { id: videogame_id } })
+
+        return res.json(
+            {
+                success: true,
+                message: "Videogame updated",
+                data: updateVideogame
+            }
+        );
+    } catch (error) {
         return res.status(500).json(
             {
                 success: false,
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -8,6 +8,8 @@ const router = require('express').Router();
 
 router.get('/profile/getallusers', verifyToken, isAdmin, userController.getAllUsers);
 router.post('/addvideogame', verifyToken, isAdmin, userController.addVideogame);
+router.put('/updatevideogame/:id', verifyToken, isAdmin, userController.updateVideogame);
+
 
 
 module.exports = router;
